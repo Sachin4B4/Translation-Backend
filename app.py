2576e34d325f2be207263969529b4c20de420699
invinayak007 <34786951+invinayak007@users.noreply.github.com>
2024-10-07 21:29:14 +0530

    Port update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def prepare_flask_request(request):
         'https': 'on', #if request.scheme == 'https' else 'off',
         'http_host': request.host,
         'script_name': request.path,
-        'server_port': '5000', #request.host.split(':')[1] if ':' in request.host else '80',
+        'server_port': 5000, #request.host.split(':')[1] if ':' in request.host else '80',
         'get_data': request.args.copy(),
         'post_data': request.form.copy(),
     }
