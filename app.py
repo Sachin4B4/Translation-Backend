9d1a187798c84b105f5a1a0e82fd9c116d63249a
invinayak007 <34786951+invinayak007@users.noreply.github.com>
2024-10-09 14:03:11 +0530

    VIQ info

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def login_callback():
      # Convert user_data to a query string
         query_string = urllib.parse.urlencode(user_data)
         # Redirect to the React dashboard with user data
-        return redirect(f'http://localhost:5173/dashboard?{query_string}')
+        return redirect(f'https://jolly-sea-03e4a990f.5.azurestaticapps.net/dashboard?{query_string}')
     else:
         return f"Error in SAML Authentication: {errors}", 500
         
